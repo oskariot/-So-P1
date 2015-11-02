b218235e596eabdf763aa68a3a022aeb4883d1d0
oskarito <oskariot@wp.pl>
2015-11-02 16:23:08 +0100

    pthread_create() and _join() with check added

--- P1.c
+++ P1.c
@@ -12,5 +12,17 @@ int main()
   pthread_t HelloWorld;
   int tret;
 
+  if (tret = pthread_create(&HelloWorld, NULL, print_hello, NULL))
+  {
+    fprintf(stderr,"Error from pthread_create(). Returned %i instead of 0.", tret);
+    exit(EXIT_FAILURE);
+  }
+
+  if (tret = pthread_join(HelloWorld, NULL))
+  {
+    fprintf(stderr, "Error from pthread_join(). Returned %i instead of 0.", tret);
+    exit(EXIT_FAILURE);
+  }
+
   return 0;
 }
