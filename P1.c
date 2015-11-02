e6da015a4f10b1c86757de2ad09f85a418ab9001
oskarito <oskariot@wp.pl>
2015-11-02 16:14:24 +0100

    main, print_hello functions added

--- P1.c
+++ P1.c
@@ -2,3 +2,15 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+void * print_hello(void * args)
+{
+  printf("Hello world, from the 2nd thread!\n");
+}
+
+int main()
+{
+  pthread_t HelloWorld;
+  int tret;
+
+  return 0;
+}
